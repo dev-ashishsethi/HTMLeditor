1a55efb0771b80b8f50aaa0620c4f5c59e59f357
Ashish Sethi <ashish.sethi@sociometrik.com>
2023-06-19 22:35:00 +0530

    feat: added html editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
 import './App.css'
-
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import { useState } from 'react'
 function App() {
-	const [count, setCount] = useState(0)
+	const [html, setHTML] = useState('')
+	const [showhtml, setShowHTML] = useState(false)
 
-	return <></>
+	function handleSave() {
+		setShowHTML(true)
+	}
+	return (
+		<>
+			<div className='App'>
+				<h2>Using CKEditor 5 build in React</h2>
+				<CKEditor
+					editor={ClassicEditor}
+					data='<p>Hello from CKEditor 5!</p>'
+					onReady={(editor) => {
+						// You can store the "editor" and use when it is needed.
+						console.log('Editor is ready to use!', editor)
+					}}
+					onChange={(event, editor) => {
+						const data = editor.getData()
+						setHTML(data)
+						console.log({ data })
+					}}
+				/>
+				<button onClick={handleSave}>Save</button>
+				<div>{showhtml && html}</div>
+			</div>
+		</>
+	)
 }
 
 export default App
